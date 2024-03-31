7f41a722b6c7f58685422cc5c85d403d7a7dab8b
lilisangma <161695518+lilisangma@users.noreply.github.com>
2024-03-31 17:03:16 +0800

    class work

---
+++ script.js
@@ -0,0 +1,8 @@
+function sayHello(){
+  var response = prompt("Which ocean identity have you decided to choose?");
+  alert('Hello,'+response + ',I hope you have a wonderful trip!!');
+}
+function showTopic(){
+  var x = document.getElementById('demo');
+
+}
